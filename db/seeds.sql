59660c7f2c11511ddd8cf40296580fca1c75afea
Col Smith <colinjcsmith@gmail.com>
2020-11-15 10:19:04 +1100

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('The Canada Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Frankiez Special', true);
+INSERT INTO burgers (name, eaten) VALUES ('Bare Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Cure Burger', true);
+INSERT INTO burgers (name) VALUES ('Son of a Cockfighter');
