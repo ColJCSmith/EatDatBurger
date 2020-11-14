979ef87830ed8f05d4c9142bd5188079de452418
Col Smith <colinjcsmith@gmail.com>
2020-11-15 10:45:16 +1100

    Title Changes

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -3,11 +3,12 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-  cat.all(function (data) {
+  // burger or burgers?
+  burger.all(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -16,8 +17,9 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/cats", function (req, res) {
-  cat.create([
+router.post("/api/burgers", function (req, res) {
+  // burger or burgers?
+  burger.create([
     "name", "eaten"
   ], [
     req.body.name, req.body.eaten
@@ -27,12 +29,12 @@ router.post("/api/cats", function (req, res) {
   });
 });
 
-router.put("/api/cats/:id", function (req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
-
-  cat.update({
+  // burger or burgers?
+  burger.update({
     eaten: req.body.eaten
   }, condition, function (result) {
     if (result.changedRows == 0) {
@@ -44,10 +46,10 @@ router.put("/api/cats/:id", function (req, res) {
   });
 });
 
-router.delete("/api/cats/:id", function (req, res) {
+router.delete("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
-  cat.delete(condition, function (result) {
+  burger.delete(condition, function (result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
+$(function () {
+  $(".change-sleep").on("click", function (event) {
     var id = $(this).data("id");
     var newSleep = $(this).data("newsleep");
 
@@ -9,11 +9,11 @@ $(function() {
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newSleepState
     }).then(
-      function() {
+      function () {
         console.log("changed sleep to", newSleep);
         // Reload the page to get the updated list
         location.reload();
@@ -21,7 +21,7 @@ $(function() {
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+  $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
@@ -31,11 +31,11 @@ $(function() {
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
       data: newCat
     }).then(
-      function() {
+      function () {
         console.log("created new cat");
         // Reload the page to get the updated list
         location.reload();
@@ -43,14 +43,14 @@ $(function() {
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
+  $(".delete-cat").on("click", function (event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(
-      function() {
+      function () {
         console.log("deleted cat", id);
         // Reload the page to get the updated list
         location.reload();
