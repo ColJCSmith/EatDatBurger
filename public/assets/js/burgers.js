27f640d1386777e0cd75819f917c7baddf112547
Col Smith <colinjcsmith@gmail.com>
2020-11-15 13:38:56 +1100

    working buttons

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  $(".change-sleep").on("click", function (event) {
+  $(".devour-it").on("click", function (event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var eatenBurger = $(this).data("eatenBurger");
 
-    var newSleepState = {
-      sleepy: newSleep
+    var newEatenBurger = {
+      eaten: eatenBurger
     };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newEatenBurger
     }).then(
       function () {
-        console.log("changed sleep to", newSleep);
+        console.log("changed eaten to", eatenBurger);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -25,18 +25,18 @@ $(function () {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
-      name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+    var newBurger = {
+      name: $("#burg").val().trim(),
+      eaten: $("[name=sleepy]:checked").val().trim()
     };
 
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function () {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
